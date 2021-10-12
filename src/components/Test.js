fdc5c4a30b7be5f28d66fb5b09b41ea84017edc6
Mohamed Essam <mo.essam.js@gmail.com>
2021-10-12 20:46:49 +0200

    ADD: Sections For Tesla

---
+++ src/components/Test.js
@@ -0,0 +1,16 @@
+title="Model-S"
+        description="Order Online For Touchless Delivery"
+        backgroundImg="model-s.jpg"
+        leftBtnText="Custom Order"
+        rightBtnText="Existing Inventory"
+
+
+        title="Model-Y"
+        description="Order Online For Touchless Delivery"
+        backgroundImg="public\images\model-y.jpg"
+        leftBtnText="Custom Order"
+        rightBtnText="Existing Inventory"
+
+
+
+        title="Model 3"
